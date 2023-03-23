007d8335037dcf4388e2486d042f91db034aa1b7
Tomáš Žilínek <xzilinek@fi.muni.cz>
2023-03-23 23:15:16 +0100

    Implemented requirements from Gitlab comments
    Csv files are appended with the date of downloading.
    Multiple methods are controlled by date.
    Created some tests for the new methods.

---
+++ ArkDiffDisplayer/ArkDiffDisplayer.Tests/FileManagementTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using ArkDiffDisplayer.Parser;
+using ArkDiffDisplayer.FileManagement;
+using System;
+
+namespace ArkDiffDisplayer.Tests
+{
+    public class FileManagementTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        
+        public void TestCsvFileDownloaded()
+        {
+            FileManagementUtils.DownloadHoldingsCsv();
+            Assert.DoesNotThrow(() => FileManagementUtils.ReadHoldingsCsvFile(DateTime.Today));
+        }
+
+
+        [TestCase("holdings.csv", 10, 2, 2023, "holdings_10.2.2023.csv")]
+        [TestCase("holdings", 5, 8, 1998, "holdings_5.8.1998")]
+        [TestCase(".csv", 7, 6, 2020, "_7.6.2020.csv")]
+        [TestCase("somefile", 2, 2, 2222, "somefile_2.2.2222")]
+        public void TestAppendDateToFileName(string fileName, int day, int month, int year, string correct)
+        {
+            string appended = FileManagementUtils.AppendDateToFileName(fileName, new DateTime(year, month, day));
+            
+            Assert.AreEqual(appended, correct);
+        }
+
+        [TestCase("ARK_INNOVATION_ETF_ARKK_HOLDINGS_10.2.2023.csv", 10, 2, 2023, true)]
+        [TestCase("ARK_INNOVATION_ETF_ARKK_HOLDINGS_5.8.1998", 5, 8, 1998, true)]
+        [TestCase("ARK_INNOVATION_ETF_ARKK_HOLDINGS_2.2.2222.csv", 2, 2, 2222, true)]
+        [TestCase("7.6.2020.csv", 7, 6, 2020, false)]
+        [TestCase("ARK_INNOVATION_ETF_ARKK_HOLDINGS_2.2222.csv", 2, 2, 2222, false)]
+        [TestCase("ARK_INNOVATION_ETF_ARKK_HOLDINGS_2.2.csv", 2, 2, 2222, false)]
+        public void TestExtractDateFromFileName(string fileName, int day, int month, int year, bool isCorrect)
+        {
+            bool success = FileManagementUtils.ExtractDateFromHoldingsCsvFileName(fileName, out DateTime date);
+
+            Assert.AreEqual(success, isCorrect);
+
+            if (success)
+            {
+                Assert.AreEqual(date.Day, day);
+                Assert.AreEqual(date.Month, month);
+                Assert.AreEqual(date.Year, year);
+            }
+        }
+    }
+}
--- ArkDiffDisplayer/ArkDiffDisplayer/FileManagement/FileManagementUtils.cs
+++ ArkDiffDisplayer/ArkDiffDisplayer/FileManagement/FileManagementUtils.cs
@@ -3,54 +3,136 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: InternalsVisibleTo("ArkDiffDisplayer.Tests")]
 namespace ArkDiffDisplayer.FileManagement
 {
     public static class FileManagementUtils
     {
         public static readonly string HoldingsCsvUrl = "https://ark-funds.com/wp-content/uploads/funds-etf-csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv";
         public static readonly string HoldingsCsvFileName = "ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv";
-        public static readonly string HoldingsCsvFileLocation = AppDomain.CurrentDomain.BaseDirectory;
+        public static readonly string HoldingsCsvFileLocation = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "holdings_files");
 
         public static bool DownloadHoldingsCsv(bool verbose = false)
         {
-            using (HttpClient client = new())
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Add("User-Agent", "Other");
+
+            try
             {
-                client.DefaultRequestHeaders.Add("User-Agent", "Other");
+                CreateCsvHoldingsFilesFolderIfNotExists();
 
-                try
-                {
-                    string fileLocationAndName = GetFileLocationAndName();
-                    string content = client.GetStringAsync(HoldingsCsvUrl).Result;
+                string fileLocationAndName = GetFileLocationAndName(DateTime.Today);
+                string content = client.GetStringAsync(HoldingsCsvUrl).Result;
 
-                    File.WriteAllText(fileLocationAndName, content);
-                    return true;
-                }
-                catch (Exception ecx)
-                {
-                    if (verbose)
-                        Console.WriteLine(ecx.Message);
+                File.WriteAllText(fileLocationAndName, content);
+                return true;
+            }
+            catch (Exception ecx)
+            {
+                if (verbose)
+                    Console.WriteLine(ecx.Message);
 
-                    return false;
-                }
+                return false;
             }
         }
 
-        public static string ReadHoldingsCsvFile()
+        public static string ReadHoldingsCsvFile(DateTime date)
         {
-            return File.ReadAllText(GetFileLocationAndName());
+            string fileLocationAndName = GetFileLocationAndName(date);
+
+            if (!File.Exists(fileLocationAndName))
+                throw new FileNotFoundException();
+            
+            return File.ReadAllText(fileLocationAndName);
         }
 
-        public static List<string> ReadLinesHoldingsCsvFile()
+        public static List<string> ReadLinesHoldingsCsvFile(DateTime date)
         {
-            return File.ReadAllLines(GetFileLocationAndName()).ToList();
+            string fileLocationAndName = GetFileLocationAndName(date);
+
+            if (!File.Exists(fileLocationAndName))
+                throw new FileNotFoundException();
+
+            return File.ReadAllLines(fileLocationAndName).ToList();
         }
 
-        private static string GetFileLocationAndName()
+        internal static string GetFileLocationAndName(DateTime date)
         {
-            return Path.Combine(HoldingsCsvFileLocation, HoldingsCsvFileName);
+            return Path.Combine(HoldingsCsvFileLocation,
+                                AppendDateToFileName(HoldingsCsvFileName, date));
+        }
+
+
+        /// <param name="fileName">May contain the .csv suffix</param>
+        internal static string AppendDateToFileName(string fileName, DateTime date)
+        {
+            int index = fileName.LastIndexOf(".csv");
+            string dateAsString = DateToString(date);
+
+            if (index == -1 || index != fileName.Length - 4)
+                return fileName + "_" + dateAsString;
+
+            return fileName.Remove(index, 4) + "_" + dateAsString + ".csv";
+        }
+
+        /// <param name="holdingsFileName">A valid file name for a csv holdings file.
+        /// May contain the .csv suffix</param>
+        /// <param name="date">Datetime representing dateString parameter</param>
+        /// <returns>Whether it was successful</returns>
+        internal static bool ExtractDateFromHoldingsCsvFileName(string holdingsFileName, out DateTime date)
+        {
+            date = new DateTime();
+
+            if (!holdingsFileName.Contains(Path.GetFileNameWithoutExtension(HoldingsCsvFileName)))
+                return false;
+
+            holdingsFileName = holdingsFileName.Remove(0, HoldingsCsvFileName.Length -4 + 1);
+            int csvIndex = holdingsFileName.LastIndexOf(".csv");
+
+            if (csvIndex != -1)
+                holdingsFileName= holdingsFileName.Remove(csvIndex);
+
+            return DateStringToDateTime(holdingsFileName, out date);
+        }
+
+        /// <returns>String representing date portion of date parameter</returns>
+        internal static string DateToString(DateTime date)
+        {
+            return date.Day.ToString() + "." + date.Month.ToString() + "." + date.Year.ToString();
+        }
+
+        /// <param name="date">Datetime representing dateString parameter</param>
+        /// <returns>Whether it was successful</returns>
+        internal static bool DateStringToDateTime(string dateString, out DateTime date)
+        {
+            date = new DateTime();
+            string[] partsString = dateString.Split('.');
+
+            if (partsString.Length != 3)
+                return false;
+
+            List<int> partsInt = new();
+
+            foreach (string part in partsString)
+            {
+                if (!int.TryParse(part, out int partInt))
+                    return false;
+
+                partsInt.Add(partInt);
+            }
+
+            date = new DateTime(partsInt[2], partsInt[1], partsInt[0]);
+            return true;
+        }
+
+        private static void CreateCsvHoldingsFilesFolderIfNotExists()
+        {
+            if (!Directory.Exists(HoldingsCsvFileLocation))
+                Directory.CreateDirectory(HoldingsCsvFileLocation);
         }
     }
 }
